94c0d5c141dc6aca8701adb12ac797f4ebcd3c83
DavidGuimaraes <DavidGuimaraes@users.noreply.github.com>
2019-08-24 16:28:58 -0400

    Create header-template.php

---
+++ header-template.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PHP Project</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light" style="max-width: 75%; margin:auto">
+  			<a class="navbar-brand" href="#">Navbar</a>
+  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+    			<span class="navbar-toggler-icon"></span>
+  			</button>
+
+  			<div class="collapse navbar-collapse" id="navbarColor03">
+    			<ul class="navbar-nav mr-auto">
+      				<li class="nav-item active">
+        				<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      				</li>
+      				<li class="nav-item"><!--dropdown show-->
+      						<!--nav-link dropdown-toggle-->
+    					<a class="nav-link" data-toggle="dropdown" href="register.php" role="button" aria-haspopup="true" aria-expanded="true">Register</a>
+    					<!--<div class="dropdown-menu show" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 38px, 0px);">
+      						<a class="dropdown-item" href="#">Action</a>
+      						<a class="dropdown-item" href="#">Another action</a>
+      						<a class="dropdown-item" href="#">Something else here</a>
+      					</div>-->
+  					</li>
+      				<li class="nav-item">
+        				<a class="nav-link" href="search.php">Search</a>
+      				</li>
+      				<li class="nav-item">
+        				<a class="nav-link" href="about.php">About</a>
+      				</li>
+    			</ul>
+  			</div>
+		</nav>
